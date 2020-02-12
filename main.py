ffd20840a3d0f61fa89e1564cb07fc2aa6349dda
Ragnar <ragnarwhitewolf@pm.me>
2020-02-12 10:00:29 +0000

    Main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-# PAGINA WEB PERSONAL
-# ESTE CODIGO ESTA BAJO LA LICENCIA TLUPL, DISPONIBLE EN EL ARCHIVO "LICENSE.MD"
-
 import os
 from flask import Flask
 
 # INICIALIZACION DE LA CLASE APP
 app = Flask("Main")
+
+# VARIABLES
+
+# FUNCIONES
+
+# PAGINAS ESTATICAS
+@app.route("/")
+def index():
+  return "WIP"
+
+# PAGINAS DINAMICAS
+
+# INICIO DEL SERVIDOR
+app.run("0.0.0.0",8080)
